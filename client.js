1b2a707432a720f3633b42c766ab24d5980e439d
= <=>
2021-03-10 17:23:16 +0100

    Upload resource to github

---
+++ client.js
@@ -0,0 +1,198 @@
+let selectedEntity = null
+
+let cam = null
+
+let ticker = null
+let toggle = false
+
+RegisterCommand(
+  'setveh',
+  (src, args, raw) => {
+    let veh = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+
+    if (!veh) {
+      log('veh not found')
+      clearTick(ticker)
+      ticker = null
+      return
+    }
+
+    log('Setting entity')
+    setEntity(veh)
+
+    log('Setting tick')
+    if (ticker != null) {
+      clearTick(ticker)
+      ticker = null
+    }
+    ticker = setTick(() => {
+      DisableControlAction(2, 80, true)
+
+      if (IsDisabledControlJustReleased(0, 80)) {
+        if (!toggle) {
+          _focusOn()
+        } else {
+          _focusOff()
+        }
+        toggle = !toggle
+      }
+    })
+  },
+  false
+)
+
+RegisterCommand(
+  'putped',
+  (src, args, raw) => {
+    log('called put ped...')
+
+    if (
+      !selectedEntity ||
+      !DoesEntityExist(selectedEntity) ||
+      !IsEntityAVehicle(selectedEntity)
+    ) {
+      log('Please select a vehicle first (/setveh)')
+    }
+
+    log('putting ped...')
+
+    let pedHash = GetHashKey('s_f_y_cop_01')
+
+    RequestModel(pedHash)
+
+    let loadCnt = 0
+    let cancel = false
+
+    while (!HasModelLoaded(pedHash) && !cancel) {
+      loadCnt++
+      if (loadCnt > 10) {
+        cancel = true
+        return
+      }
+      Wait(500)
+    }
+
+    if (cancel) {
+      log('Could not load model within 5s')
+      return
+    }
+
+    let ped = CreatePedInsideVehicle(selectedEntity, 4, pedHash, -1, true, true)
+
+    SetBlockingOfNonTemporaryEvents(ped)
+    TaskVehicleDriveWander(ped, selectedEntity, 35, 2883621)
+
+    log('put ped in veh')
+  },
+  false
+)
+
+function _setEntity(entity) {
+  /**/
+  if (selectedEntity != null) {
+    if (toggle) {
+      _focusOff()
+    }
+  }
+  /**/
+  selectedEntity = entity
+}
+
+let t_UpdateCamPos = null
+function _focusOn() {
+  // should never be called, but still leaving "just in case" :p
+  //if (focused) _focusOff()
+
+  if (selectedEntity == null) {
+    log('Tried to focus, but entity is invalid')
+    clearTick(ticker)
+    _focusOff()
+    return
+  }
+
+  if (!DoesCamExist(cam)) {
+    cam = CreateCameraWithParams(
+      //26379945,
+      'DEFAULT_SCRIPTED_CAMERA',
+      GetGameplayCamCoord(),
+      GetGameplayCamRot(2),
+      GetGameplayCamFov(),
+      true,
+      2
+    )
+  }
+
+  PointCamAtEntity(cam, selectedEntity, 0.0, 0.0, -0.2, false)
+  SetCamFov(cam, 40)
+
+  if (t_UpdateCamPos != null) clearTick(t_UpdateCamPos)
+  t_UpdateCamPos = setTick(updateCam)
+
+  SetCamActive(cam, true)
+  RenderScriptCams(true, 1, 500, true, false, false)
+  AnimpostfxPlay('ChopVision', 0, true)
+}
+
+const radius = 25
+const heightLift = 12
+function updateCam() {
+  let [px, py, pz] = GetEntityCoords(GetPlayerPed(-1), false)
+  let [ex, ey, ez] = GetEntityCoords(selectedEntity, false)
+  const eRot = GetEntityRotation(GetPlayerPed(-1), 2)[2]
+
+  let anglePane = (Math.atan2(px - ex, py - ey) * 180) / Math.PI
+  let angleAlt = (Math.atan2(pz - ez, px - ex) * 180) / Math.PI
+
+  let camPosX = px + Sin(anglePane) * radius,
+    camPosY = py + Cos(anglePane) * radius,
+    camPosZ = pz + Sin(angleAlt) * heightLift * 2
+
+  camPosX -= Cos(eRot + 90) * Cos(angleAlt) * 2
+  camPosY -= Sin(eRot + 90) * Cos(angleAlt) * 2
+
+  console.log({ px, py, pz, anglePane, angleAlt, camPosX, camPosY, camPosZ })
+
+  SetCamCoord(cam, camPosX, camPosY, pz + heightLift)
+  console.log(GetCamCoord(cam))
+
+  // add controls for FOV
+}
+
+function _focusOff() {
+  // disable cinematic cam
+  if (DoesCamExist(cam)) {
+    // destroy cam
+    SetCamActive(cam, false)
+    DestroyCam(cam, false)
+    cam = null
+  }
+
+  clearTick(t_UpdateCamPos)
+  t_UpdateCamPos = null
+
+  //SetNightvision(false)
+  AnimpostfxStop('ChopVision')
+  RenderScriptCams(false, 1, 500, true, false, false)
+}
+
+// not working smh
+/**/
+function focus(toggle) {
+  if (!DoesEntityExist(selectedEntity))
+    log('Please set a valid entity firstly (exports: setEntity)')
+  if (toggle) _focusOn()
+  else _focusOff()
+}
+
+function setEntity(entity) {
+  if (DoesEntityExist(entity)) _setEntity(entity)
+  else log('Received invalid entity')
+}
+
+/**/
+
+function log(txt) {
+  emit('chat:addMessage', {
+    args: [txt],
+  })
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'locomoco#4739'
+description 'Cinematic camera test'
+version '0.3.1'
+
+client_script 'client.js'
